ef07145bd7e90299e662a52d81c72aafed8e07bb
romaric.guth <romaric.guth@epita.fr>
2022-03-18 15:30:56 +0100

    compute linear dataset for each dataset - for tests

--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -44,6 +44,29 @@ export default {
     };
   },
   mounted() {
+    const { datasets, labels } = this.chartData;
+    datasets.forEach((dataset) => {
+      /* compute average linear coefficient from data points */
+      let coeffLinear = 0;
+      for (let i = 1; i < dataset.data.length; i += 1) {
+        /* sum up gaps */
+        coeffLinear += dataset.data[i] - dataset.data[i - 1] / (labels[i] - labels[i - 1]);
+      }
+      /* division to get average */
+      coeffLinear /= dataset.data.length - 1; // get average gap between 2 data points
+
+      /* construct approximated linear dataset from effective dataset */
+      const linearData = new Array(dataset.length);
+      [linearData[0]] = dataset.data;
+      for (let i = 1; i < dataset.data.length; i += 1) {
+        linearData[i] = linearData[0] + coeffLinear * (labels[i] - labels[0]);
+      }
+      datasets.push({
+        ...dataset,
+        data: linearData,
+        label: `${dataset.label} linéaire`,
+      });
+    });
     this.renderChart(this.chartData, this.options);
   },
 };
