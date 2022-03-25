a087d07e03ca6efa924b1d4b4422dc3709025f99
Ewann <ewannpv@gmail.com>
2022-03-25 10:54:43 +0100

    Fixes title

--- src/components/evolution/EvolutionContainer.vue
+++ src/components/evolution/EvolutionContainer.vue
@@ -33,27 +33,7 @@
               />
             </v-col>
           </v-row>
-          <div class="my-3">
-            <v-row class="text-h5"> Options </v-row>
-            <v-row>
-              <v-col md="6" cols="12">
-                <v-range-slider
-                  v-model="range"
-                  :max="max"
-                  :min="min"
-                  hide-details
-                  class="align-center"
-                >
-                  <template v-slot:prepend>
-                    {{ range[0] }}
-                  </template>
-                  <template v-slot:append>
-                    {{ range[1] }}
-                  </template>
-                </v-range-slider>
-              </v-col>
-            </v-row>
-          </div>
+          <EvolutionOption />
         </v-container>
       </v-card-text>
     </CardLayout>
@@ -65,6 +45,7 @@ import ContainerLayout from '@/components/layout/ContainerLayout.vue';
 import CardLayout from '@/components/layout/CardLayout.vue';
 import EvolutionCard from '@/components/evolution/EvolutionCard.vue';
 import GapCard from '@/components/evolution/GapCard.vue';
+import EvolutionOption from '@/components/evolution/EvolutionOption.vue';
 
 export default {
   components: {
@@ -72,9 +53,12 @@ export default {
     CardLayout,
     EvolutionCard,
     GapCard,
+    EvolutionOption,
   },
   data() {
     return {
+      checkbox: true,
+
       min: 2000,
       max: 2020,
       lastEstimatedYear: 2050,
---
+++ src/components/evolution/EvolutionOption.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="my-3">
+    <v-row class="text-h5"> Options </v-row>
+    <v-row>
+      <v-col md="6" cols="12">
+        <v-range-slider v-model="range" :max="max" :min="min" hide-details class="align-center">
+          <template v-slot:prepend>
+            {{ range[0] }}
+          </template>
+          <template v-slot:append>
+            {{ range[1] }}
+          </template>
+        </v-range-slider>
+      </v-col>
+    </v-row>
+    <template>
+      <v-treeview selectable :items="treeViewItems"></v-treeview>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      checkbox: true,
+      min: 2000,
+      max: 2020,
+      lastEstimatedYear: 2050,
+      range: [0, 0],
+      treeViewItems: [
+        {
+          id: 1,
+          name: 'Economie :',
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.range = [this.min, this.max];
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
