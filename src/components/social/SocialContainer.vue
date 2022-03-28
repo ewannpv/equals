a2645a1499a59b5119aa9d5f61ef3a11a2887a1f
Ewann <ewannpv@gmail.com>
2022-03-28 16:36:11 +0200

    Updates icon colors

--- src/components/social/SocialContainer.vue
+++ src/components/social/SocialContainer.vue
@@ -2,7 +2,7 @@
   <ContainerLayout class="py-3 social-container">
     <CardLayout color="white" class="mx-3 mb-5">
       <v-card-title>
-        <v-icon large left> mdi-account-circle </v-icon>
+        <v-icon large left color="#fe9292"> mdi-account-circle </v-icon>
         <span class="text-h4">Social</span>
       </v-card-title>
     </CardLayout>
